3ed2dfd42e08fa2c6bf21b5844fea648cf164fc7
xamayca <Christopher.hariouat@gmail.com>
2023-12-06 15:26:09 +0100

    fix w90 section

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,20 @@ menuCloseHtml.addEventListener('click', () => {
     document.body.classList.toggle('overflow-auto');
 });
 
+const hoverImages = [
+    './assets/img/ico-ear-hover.svg',
+    './assets/img/ico-bulb-hover.svg',
+    './assets/img/ico-valid-hover.svg'
+];
+
+document.querySelectorAll('.card').forEach((card, index) => {
+    const img = card.querySelector('img');
+    img.dataset.originalSrc = img.src;
+
+    card.addEventListener('mouseenter', () => {
+        img.src = hoverImages[index];
+    });
+    card.addEventListener('mouseleave', () => {
+        img.src = img.dataset.originalSrc;
+    });
+});
